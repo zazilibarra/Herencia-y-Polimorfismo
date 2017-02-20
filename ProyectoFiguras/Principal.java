92b2d145b1722bbce164fda3c5697ca36d5fa805
GiiL <giil.guerrero@hotmail.com>
2017-02-20 12:04:48 -0600

    Clase Figura y Triangulo
    Se crea una clase base de la cual heredan las demas figuras

---
+++ ProyectoFiguras/Principal.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Principal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Principal
+{
+   public static void main(String [] args)
+   {
+       Triangulo t1 = new Triangulo(3, 4);
+       t1.calculaArea();
+       System.out. println(t1.accedeArea());
+    }
+}
