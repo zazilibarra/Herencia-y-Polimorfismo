1722803b3432bfe75afd5130a37824da7dc899d0
Zazil <zaziil.97@gmail.com>
2017-03-10 12:14:45 -0600

    Start-Stop

--- BotaPelota/Lienzo.java
+++ BotaPelota/Lienzo.java
@@ -2,30 +2,23 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.Timer;
-public class Lienzo extends JPanel
+
+public class Lienzo extends JPanel //implements KeyListener
 {
     private Pelota pelota;
     
     public Lienzo()
     {
         pelota = new Pelota(100, 100, 15);
-        EscuchadorTiempo escuchaTiempo = new EscuchadorTiempo();
-        Timer tiempo = new Timer(10, escuchaTiempo);
-        tiempo.start();
+        //this.addKeyListener(this);
+    }
+       
+    public void actua()
+    {
+        pelota.muevete(this.getBounds());
+        this.repaint();
     }
     
-     class EscuchadorTiempo implements ActionListener
-    {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-            //System.out.println("Hola Pelota");
-            pelota.muevete(getBounds());
-            repaint();
-        }
-    }
-   
     @Override
     public void paintComponent(Graphics g)
     {
--- BotaPelota/Ventana.java
+++ BotaPelota/Ventana.java
@@ -1,13 +1,69 @@
 import javax.swing.JFrame;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class Ventana extends JFrame
 {
     private Lienzo dibujo;
+    private Timer tiempo;
     
     public Ventana(int ancho, int alto)
     {
         this.setSize(ancho, alto);
         dibujo = new Lienzo();
+        //dibujo.addKeyListener(dibujo);
         this.add(dibujo);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        EscuchadorTeclado escuchadorTeclado = new EscuchadorTeclado();
+        this.addKeyListener(escuchadorTeclado);
+        
+        EscuchadorTiempo escuchaTiempo = new EscuchadorTiempo();
+        tiempo = new Timer(1, escuchaTiempo);
+        tiempo.start();
+    }  
+    
+    class EscuchadorTiempo implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            //System.out.println("Hola Pelota");
+            //pelota.muevete(getBounds());
+            //repaint();
+            dibujo.actua();
+        }
+    }
+    
+    class EscuchadorTeclado implements KeyListener
+    {
+        @Override
+        public void keyPressed(KeyEvent e)
+        {
+            if(e.getKeyChar() == 'd')
+            {
+                tiempo.stop();
+            }
+            else if(e.getKeyChar() == 'i')
+            {
+                tiempo.start();
+            }
+            //System.out.println("keyPressed: " + e.getKeyChar());
+        }
+        
+        @Override
+        public void keyReleased(KeyEvent e)
+        {
+            //System.out.println("keyReleased : " + e.getKeyChar());
+        }
+        
+        @Override
+        public void keyTyped(KeyEvent e)
+        {
+            //System.out.println("keyTyped : " + e.getKeyChar());
+        }
     }
 }
